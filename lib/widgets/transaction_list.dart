d0d15c6a3971199f5d30c78b9d9b01586ff7bc5e
Bezoyan <zoya1905bezoyan@gmail.com>
2020-03-19 13:14:30 +0400

    Add text to delete button depending on device size

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -57,11 +57,18 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(transactions[index].date),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => deleteTx(transactions[index]),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 360
+                      ? FlatButton.icon(
+                          icon: Icon(Icons.delete),
+                          textColor: Theme.of(context).errorColor,
+                          label: Text('Delete'),
+                          onPressed: () => deleteTx(transactions[index]),
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transactions[index]),
+                        ),
                 ),
               );
             },
